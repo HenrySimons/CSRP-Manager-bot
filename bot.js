e2f05a78f7be70f093f2fe2facb68365160cb345
HenrySimons <44886621+HenrySimons@users.noreply.github.com>
2018-11-09 15:18:06 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,22 +7,7 @@ client.on('ready', () => {
 
 client.on('message', message => {
     if (message.content === '!veh') {
-    	message.reply(':large_blue_diamond: Police Vehicles :large_blue_diamond: 
-police    =    Ford Crown Victoria              | 7 Liveries
-police2  = Dodge Charger                          | 6 Liveries 
-police3  = Vanilla Police                              | 0 Liveries 
-police4  = Unmarked Ford Taurus             | 0 Liveries
-police5  = Ford Utility (LSPD)                    | 0 Liveries 
-police6  = Chevy Tahoe (K9)  (LSPD)      | 0 Liveries 
-police7  = Ford Crown Victoria (LSPD)    | 0 Liveries
-police8  = Chevrolet Impala (LSPD)          | 0 Liveries
-sheriff    = Dodge Charger (BCSO)             | 0 Liveries
-sheriff2  = Chevy Tahoe     (BCSO)             | 0 Liveries
-sheriff3  =  Police Interceptor (BSCO)       | 0 Liveries 
-fbi            = Unmarked Dodge Charger       | 0 Liveries
-fbi2          = broken N/A                              | 0 Liveries
-hwaycar = Police Interceptor (SAHP)      | 0 Liveries
-hwaycar3= Ford Interceptor Utility  (ST) |0 Liveries ');
+    	message.reply(':large_blue_diamond: Police Vehicles :large_blue_diamond:');
   	}
 });
 
